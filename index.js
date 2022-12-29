6516cda0ad79c4150fad76b9e5188b6d51d48226
Ruben Rubio <ruben.rubio.salazar@outlook.com>
2022-12-29 17:50:17 -0600

    Seccion 12 Google

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const app = express();
 //Configurar CORS
 app.use(cors());
 
+//Carpeta publica
+app.use(express.static('public'));
+
 //Lectura y parse del body
 app.use(express.json());
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,24 @@ Ruta : /api/login
 */
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth");
+const { login, loginGoogle } = require("../controllers/auth");
 const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
 
 router.post('/',
     [
-         check('password', 'El password es obligatorio').not().isEmpty(),
-         check('email', 'El email es obligatorio').isEmail(),
-         validarCampos,
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos,
     ],
     login);
+
+router.post('/google',
+    [
+        check('token', 'El token de google es obligatorio').not().isEmpty(),
+        validarCampos,
+    ],
+    loginGoogle);
 
 module.exports = router;
